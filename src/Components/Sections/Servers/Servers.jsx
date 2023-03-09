7e30d21a1df6ce5a73477a80372de484233f22af
Ian Main <imitchell.main@gmail.com>
2023-03-09 14:03:36 -0800

    Update Servers.jsx

--- src/Components/Sections/Servers/Servers.jsx
+++ src/Components/Sections/Servers/Servers.jsx
@@ -4,22 +4,22 @@ import { Link } from "react-router-dom";
 
 const Servers = () => {
   return (
-    <section className="xxs:py-16 xxs:px-10 lg:px-0 lg:py-0 xxs:bg-neutral lg:pb-10 lg:bg-transparent">
+    <section className="xxs:py-16 xxs:px-10 lg:px-0 lg:py-0 xxs:dark:bg-neutral bg-white lg:pb-10 lg:bg-transparent">
       <video
         src={gaming}
         autoPlay
         loop
         muted
-        className="lg:w-full lg:h-full object-cover absolute -z-10 brightness-50 xxs:w-[200px]"
+        className="lg:w-full lg:h-full object-cover absolute -z-10 brightness-50 xxs:w-[200px] invisible lg:visible"
       />
       <h1 className="text-4xl font-extrabold text-center text-green-400 uppercase mx-auto p-3">
         Game Servers
       </h1>
-      <h1 className="text-3xl text-white mb-4 tracking-tight font-extrabold text-center">
+      <h1 className="text-3xl lg:text-white xxs:text-black dark:text-white mb-4 tracking-tight font-extrabold text-center">
         {" "}
         Get access to the latest and greatest games on our servers!
       </h1>
-      <p className="text-center text-white text-xl mx-auto tracking-tight max-w-[700px] p-3">
+      <p className="text-center lg:text-white xxs:text-black dark:text-white text-xl mx-auto tracking-tight max-w-[700px] p-3">
         We host a variety of game servers for you to play on. We host Minecraft,
         Ark Survival Evolved, and more! To get more information or just looking
         to hang out with us, join our Discord!
@@ -28,7 +28,7 @@ const Servers = () => {
       {/* Card Grid */}
       <div className="grid lg:grid-cols-2 xxs:grid-cols-1 xxxs:grid-col-1 mt-8 mx-auto max-w-[1240px] gap-6 ">
         {/* Card 1 */}
-        <div className="card lg:w-[450px] bg-base-100 shadow-md shadow-green-400 lg:mx-auto xxs:w-70">
+        <div className="card lg:w-[450px] dark:bg-base-100 bg-white border-2 dark:border-none shadow-md shadow-green-400 lg:mx-auto xxs:w-70">
           <figure>
             <img
               src="https://altarofgaming.com/ark-survival-evolved-beginners-guide/castoroides/"
@@ -54,7 +54,7 @@ const Servers = () => {
         </div>
 
         {/* Card 2 */}
-        <div className="card lg:w-[450px] bg-base-100 shadow-md shadow-yellow-300 lg:mx-auto xxs:w-70">
+        <div className="card lg:w-[450px] dark:bg-base-100 bg-white shadow-md shadow-yellow-300 border-2 dark:border-none lg:mx-auto xxs:w-70">
           <figure>
             <img
               src="https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2020/4/28/bjoyslzjb3uxqyg82uz2/minecraft"
